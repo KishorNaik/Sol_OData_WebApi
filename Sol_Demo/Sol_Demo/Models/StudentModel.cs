3ab876fba58c466278746535de44d947e9b2b934
Kishor Naik <kishor.naik011.net@outlook.com>
2020-09-02 20:51:49 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Models
+{
+    public class StudentModel
+    {
+        public String Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
